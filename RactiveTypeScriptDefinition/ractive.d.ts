6bbbb4840fafe1acde23daed9cea98bcc0f58aa5
Han Lin Yap <codler@gmail.com>
2014-08-31 23:17:44 +0200

    Update definition to match Ractive 0.5.5

--- RactiveTypeScriptDefinition/ractive.d.ts
+++ RactiveTypeScriptDefinition/ractive.d.ts
@@ -1,8 +1,8 @@
-﻿// Type definitions for Ractive 0.4.0
+﻿// Type definitions for Ractive 0.5.5
 // Project: http://ractivejs.org
 // Definitions by: Han Lin Yap <http://yap.nu>
 // Definitions: https://github.com/codler/Ractive-TypeScript-Definition
-// Version: 0.4.0-7+2014-07-25
+// Version: 0.5.5-1+2014-08-31
 
 // It's functionally identical to the ES6 promise (as currently spec'd) except that Promise.race and Promise.cast are not currently implemented.
 interface RactivePromise extends Object {
@@ -11,6 +11,10 @@ interface RactivePromise extends Object {
 
 interface RactiveAnimationPromise extends RactivePromise {
 	stop(): void; // TODO: void?
+}
+
+interface RactiveAdaptorPlugin extends Object {
+	// TODO: 
 }
 
 interface RactiveComponentPlugin extends RactiveStatic {
@@ -31,6 +35,10 @@ interface RactiveEventPlugin extends Function {
 
 interface RactiveTransitionPlugin {
 	(t: RactiveTransition, params: Object): void;
+}
+
+interface RactiveAdaptorPlugins {
+	[key: string]: RactiveAdaptorPlugin;
 }
 
 interface RactiveComponentPlugins {
@@ -126,32 +134,54 @@ interface RactiveSanitizeOptions {
 }
 
 interface RactiveNewOptions {
-	// TODO: adaptors or adapt? Confused in documentation
-	//adapt?: any[];
+	/**
+	 * @type List of mixed string or Adaptor
+	 */
+	adapt?: any[];
+	
+	adaptors?: RactiveAdaptorPlugins;
 
+	/**
+	 * Default false
+	 * @type boolean or any type that option `el` accepts (HTMLElement or String or jQuery-like collection)
+	 */
+	append?: any;
+	
 	complete?: Function;
 	components?: RactiveComponentPlugins;
 	computed?: Object;
+	// Since 0.5.5
+	// TODO: unclear in documantation
+	css?: string;
+	
 	/**
-	 * TODO: Question - When is data Array?
+	 * TODO: Question - When is data Array or String?
 	 *
-	 * @type Object or Array
+	 * @type Object, Array, String or Function
 	 */
+	// TODO: undocumented type Function
 	data?: any;
 
-	// TODO: undocumented in Initialisation options page
 	decorators?: RactiveDecoratorPlugins;
-
 	/**
 	 * @type [open, close]
 	 */
 	delimiters?: string[];
+	
+	// TODO: String or Function
+	easing?: any;
+	
 	/**
 	 * @type HTMLElement or String or jQuery-like collection
 	 */
 	el?: any;
 	// TODO: undocumented in Initialisation options page
 	events?: RactiveEventPlugins;
+	
+	// Since 0.5.5
+	// TODO: unclear in documantation
+	interpolators?: { [key: string]: any; };
+	
 	/**
 	 * any is same type as template
 	 */
@@ -162,6 +192,16 @@ interface RactiveNewOptions {
 	 */
 	sanitize?: any;
 	/**
+	 * Default ['[[', ']]']
+	 * @type [open, close]
+	 */
+	staticDelimiters?: string[];
+	/**
+	 * Default ['[[[', ']]]']
+	 * @type [open, close]
+	 */
+	staticTripleDelimiters?: string[];
+	/**
 	 * @type String or (if preparsing "Ractive.parse") Array or Object
 	 */
 	template?: any;
@@ -171,21 +211,23 @@ interface RactiveNewOptions {
 	 */
 	tripleDelimiters?: string[];
 
-	// TODO: In dev documentation append have anchor??
-	// Default false
-	append?: boolean;
-	// Default false
-	debug?: boolean;
 	// Default false
 	lazy?: boolean;
 	// Default false
 	magic?: boolean;
 	// Default true
 	modifyArrays?: boolean;
+	// Since 0.5.5
+	// TODO: unclear in documantation
+	// Default false
+	noCSSTransform?: boolean;
 	// Default false
 	noIntro?: boolean;
 	// Default false
 	preserveWhitespace?: boolean;
+	// Since 0.5.5
+	// Default true
+	stripComments?: boolean;
 	// Default true
 	twoway?: boolean;
 
@@ -200,8 +242,11 @@ interface RactiveExtendOptions extends RactiveNewOptions {
 	isolated?: boolean;
 }
 
-// TODO: is this correct? Not documented
+// See ractive change log "All configuration options, except plugin registries, can be specified on Ractive.defaults and Component.defaults"
 interface RactiveDefaultsOptions extends RactiveExtendOptions {
+	// TODO: not correctly documented
+	// Default false
+	debug?: boolean;
 }
 
 /**
@@ -215,16 +260,17 @@ interface RactiveStatic {
 	parse(template: string, options?: RactiveParseOptions): any;
 
 	// TODO: undocumented
-	adaptors: { [key: string]: any; };
+	adaptors: RactiveAdaptorPlugins;
 
 	// TODO: undocumented
 	components: RactiveComponentPlugins;
 
-	// TODO: undocumented
 	defaults: RactiveDefaultsOptions;
 
 	// TODO: undocumented
 	decorators: RactiveDecoratorPlugins;
+	
+	easing: { [key: string]: (x: number) => number; };
 
 	// TODO: undocumented
 	events: RactiveEventPlugins;
@@ -280,20 +326,36 @@ interface Ractive {
 	observe(map: Object, options?: RactiveObserveOptions): RactiveObserve;
 
 	// TODO: check handler type
-	off(eventName?: string, handler?: () => void): void; // TODO: void?
+	off(eventName?: string, handler?: () => void): Ractive;
 
 	// handler context Ractive
 	on(eventName: string, handler: (event?: RactiveEvent, ...args: any[]) => void): RactiveObserve;
 	// TODO: undocumented
 	on(map: { [eventName: string]: (event?: RactiveEvent, ...args: any[]) => void }): RactiveObserve;
 
-	// TODO: ???
-	//render();
+	// Since 0.5.5
+	pop(keypath: string): RactivePromise;
+	
+	// Since 0.5.5
+	push(keypath: string, value: any): RactivePromise;
+	
+	// TODO: target - Node or String or jQuery (see Valid selectors)
+	render(target: any): void; // TODO: void?
 
 	reset(data?: Object): RactivePromise;
+	
+	// Since 0.5.5
+	// TODO: undocumented, mentioned in ractive change log
+	resetTemplate(): void; // TODO: void?
 
 	set(keypath: string, value: any): RactivePromise;
 	set(map: Object): RactivePromise;
+	
+	// Since 0.5.5
+	shift(keypath: string): RactivePromise;
+	
+	// Since 0.5.5
+	splice(keypath: string, index: number, removeCount: number, ...add: any[]): RactivePromise;
 
 	subtract(keypath: string, number?: number): RactivePromise;
 
@@ -303,6 +365,9 @@ interface Ractive {
 
 	toHTML(): string;
 
+	// Since 0.5.5
+	unshift(keypath: string, value: any): RactivePromise;
+	
 	update(keypath?: string): RactivePromise;
 
 	/**
@@ -310,9 +375,7 @@ interface Ractive {
 	 * @param keypath A string
 	 * @param cascade A boolean with default false
 	 */
-	updateModel(keypath?: string, cascade?: boolean): void;
-	// TODO: In next release
-	// updateModel(keypath?: string, cascade?: boolean): RactivePromise;
+	updateModel(keypath?: string, cascade?: boolean): RactivePromise;
 
 	// Properties
 
